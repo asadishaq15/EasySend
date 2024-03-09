06e0d04dc7893b3a54b78c73983a2b76323d8eda
asadishaq15 <asadishaq392@gmail.com>
2024-03-09 12:47:07 +0500

    add custom html template fields

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const cors = require('cors');
-const multer = require('multer');
+const cors = require('cors'); // Added for path manipulation
 const app = express();
 require("dotenv").config();
 
@@ -13,55 +12,45 @@ app.use(cors());
 const transporter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
-
-    user: process.env.GMAIL_USER,
+      user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
-
   },
 });
 
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
 const port = process.env.PORT || 3001;
 
-
-app.post('/contact-form', upload.single('file'), (req, res) => {
+// Endpoint for handling contact form submissions
+app.post('/contact-form', (req, res) => {
   const formData = req.body;
-  const file = req.file; 
+  const fs = require('fs');
+  const template = fs.readFileSync('contact-form.html', 'utf8');
+
+  const formattedEmail = template
+    .replace('{{name}}', formData.name)
+    .replace('{{email}}', formData.email)
+    .replace('{{phone}}', formData.phone)
+    .replace('{{company}}', formData.company)
+    .replace('{{department}}', formData.department)
+    .replace('{{message}}', formData.message)
+    ;
 
   const mailOptions = {
     from: 'asadishaq013@gmail.com',
-    to: 'asadishaq392@gmail.com',
+    to: 'asadishaq392@gmail.com', // Replace with the contact form recipient's email address
     subject: 'Contact Us Form Submission',
-    html: `
-      <p>Name: ${formData.name}</p>
-      <p>Email: ${formData.email}</p>
-      <p>Phone: ${formData.phone}</p>
-      <p>Company: ${formData.company}</p>
-      <p>Department: ${formData.department}</p>
-      <p>Message: ${formData.message}</p>
-    `,
-    attachments: [
-      {
-        filename: file.originalname,
-        content: file.buffer
-      }
-    ]
+    html: formattedEmail,
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error('Error sending contact form data:', error);
-      res.status(500).json({ status: 'error' });
-    } else {
-      console.log('Contact form data sent successfully:', formData);
-      res.status(200).json({ status: 'success' });
-    }
-  });
+  try {
+    transporter.sendMail(mailOptions);
+    console.log('Contact form data sent successfully:', formData);
+    res.status(200).json({ status: 'success' });
+  } catch (error) {
+    console.error('Error sending contact form data:', error);
+    res.status(500).json({ status: 'error' });
+  }
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log(`Server is running on port ${port}`);
 });
